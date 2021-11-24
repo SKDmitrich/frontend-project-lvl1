ceb7a99e14d1e7b8a3fbeede7db712ecc5b19240
Sergey Kuznetsov <kuznetsovsd89@gmail.com>
2021-11-25 01:55:40 +0300

    fix: cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const userName = () => {
-  return readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default userName;
