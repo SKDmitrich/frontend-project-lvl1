ab62929d4ffe5b9372c4e29ce19db6804e06f503
Sergey Kuznetsov <kuznetsovsd89@gmail.com>
2021-11-28 11:14:17 +0300

    Add game-prime.js

---
+++ src/games/game-prime.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import { startGame } from '../index.js';
+
+// Rulles game
+const rullesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+// getTrueAnswer
+const getTrueAnswer = (question) => {
+  for (let i = 2; i < question; i += 1) {
+    if (question % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+// Generator question
+const generatorQuestion = () => {
+  const question = _.random(2, 1000);
+  const trueAnswer = getTrueAnswer(question);
+  return [question, trueAnswer];
+};
+
+const newGame = () => startGame(rullesGame, generatorQuestion);
+
+export default newGame;
